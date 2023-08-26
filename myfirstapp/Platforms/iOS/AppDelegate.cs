eaecca28342d06dd028570a6b69ffddbaa8f2af8
aceracad2 <aceracad2@DESKTOP-L8RJPSU>
2023-08-26 09:18:55 -0800

    Add project files.

---
+++ myfirstapp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace myfirstapp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
